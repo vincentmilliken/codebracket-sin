6be400fbe1873b087220827edbb63494341101a4
vincentmilliken <vincentamilliken@gmail.com>
2013-02-06 14:00:59 +0000

    updated db call

--- recall.rb
+++ recall.rb
@@ -1,16 +1,16 @@
 require 'rubygems'
 require 'sinatra'
 require 'data_mapper'
-require 'dm-core'
-require 'pg'
+require 'dm-core'  
+require 'dm-timestamps'  
+require 'dm-validations'  
+require 'dm-migrations'
 
 Dir['vendor/*'].each do |lib|
   $:.unshift(File.join(File.dirname(__FILE__), lib, 'lib'))
 end
 
-configure do
- DataMapper.setup(:default, ENV['DATABASE_URL'] || File.join("sqlite3://",settings.root, "recall.db"))
-end
+DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite3://#{Dir.pwd}/recall.db')
 
 # set :database, ENV['DATABASE_URL'] || 'sqlite3://localhost/recall.db'
 # DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/recall.db")
